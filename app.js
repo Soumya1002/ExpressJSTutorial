b41128b765c9599f594d4517286395dcf32225b0
Soumya1002 <soumyasenthil1002@gmail.com>
2024-01-19 15:03:21 +0530

    serving static files

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const app=express();
 
 
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
-
 app.use((req,res,next)=>{
     res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,18 @@
 const express=require('express');
 const router=express.Router();
 const path=require('path');
+const rootDir=require('../utils/path')
 
 router.get('/add-product',(req, res, next)=>{
     console.log("inside add product page");
-    res.sendFile(path.join(__dirname,'..','views','add-product.html'));
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
     
 })
 
 router.post('/add-product',(req, res, next)=>{
     console.log("inside store product page");
     console.log(req.body)
-    res.sendFile(path.join(__dirname,'..','views','add-product.html'));     
+    res.sendFile(path.join(rootDir,'views','add-product.html'));     
 })
 
 
